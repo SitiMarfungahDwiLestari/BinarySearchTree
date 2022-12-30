635202be1a21259f1e4d001225e75f69c8a9ed29
Siti Marfungah Dwi Lestari <siti.marfungah.ft21@mail.umy.ac.id>
2022-12-30 17:15:48 +0700

    add constructor for the class node

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -7,6 +7,14 @@ namespace BinarySearchTree
         public string info;
         public Node leftchild;
         public Node rightchild;
+        
+        //Constructor for the node class
+        public Node (string i, Node l, Node r)
+        {
+            info = i;
+            leftchild = l;
+            rightchild = r;
+        }
     }
     internal class Program
     {
