df2f7d5af5290193dc4254240b9c384ce42b66bf
Siti Marfungah Dwi Lestari <siti.marfungah.ft21@mail.umy.ac.id>
2022-12-30 17:14:18 +0700

    declare the variables

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -2,6 +2,12 @@
 
 namespace BinarySearchTree
 {
+    class Node
+    {
+        public string info;
+        public Node leftchild;
+        public Node rightchild;
+    }
     internal class Program
     {
         static void Main(string[] args)
